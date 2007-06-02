acf7df76d6cd8163912c83dc7ce2f5d468b67ca7
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2007-06-02 00:48:05 +0000

    Removed objects with no textures from the list.txt of a couple of tracks, tried out the M3 settings posted on the forum minus clutch changes, added a shaders folder.

---
+++ shaders/simple/fragment.glsl
@@ -0,0 +1,10 @@
+varying vec2 tu0coord;
+uniform sampler2D tu0_2D;
+
+void main()
+{
+	// Setting Each Pixel To Red
+	//gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	
+	gl_FragColor = texture2D(tu0_2D, tu0coord);
+}
