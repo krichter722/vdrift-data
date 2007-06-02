c64af3204389a38bb642eea9bc7b3885d24c1b9e
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2007-06-02 18:38:46 +0000

    Full shader reverted to simple shader.

--- shaders/full/vertex.glsl
+++ shaders/full/vertex.glsl
@@ -1,16 +1,12 @@
-varying vec4 oldpos;
-varying vec4 newpos;
-varying vec2 texcoord_2d;
-
-//uniform mat4 prev_modelview;
-uniform mat4 prev_modelviewproj;
+varying vec2 tu0coord;
 
 void main()
 {
-	vec4 new_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-	vec4 old_Position = prev_modelviewproj * gl_Vertex; //TODO: should use old_Vertex passed in by CPU for dynamic objects
+	// Transforming the vertex
+	gl_Position = ftransform();
 	
-	gl_Position = new_Position;
-	oldpos = old_Position;
-	newpos = new_Position;
+	// Setting the color
+	gl_FrontColor = gl_Color;
+	
+	tu0coord = vec2(gl_MultiTexCoord0);
 }
