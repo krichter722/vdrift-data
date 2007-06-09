3006a00e7d133b72423e8024f60471550e300399
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2007-06-09 04:23:11 +0000

    Bloom is now up to snuff!

--- vdrift-data/shaders/bloompass/fragment.glsl
+++ vdrift-data/shaders/bloompass/fragment.glsl
@@ -1,4 +1,5 @@
 uniform sampler2DRect tu0_2DRect;
+uniform sampler2DRect tu1_2DRect;
 varying vec2 texcoord_2d;
 uniform float bloomfactor;
 uniform float screenw;
@@ -10,9 +11,14 @@ void main()
 	tc.x *= screenw;
 	tc.y *= screenh;
 	vec4 tu0_2D_val = texture2DRect(tu0_2DRect, tc);
+	vec4 tu1_2D_val = texture2DRect(tu1_2DRect, tc);
 	//gl_FragColor = vec4(gl_FragColor.r*tu0_2D_val.r,gl_FragColor.g*tu0_2D_val.g,gl_FragColor.b*tu0_2D_val.b,0);
 	vec3 orig = tu0_2D_val.rgb;
-	vec3 blurred = orig;
-	vec3 final = orig * blurred;
+	vec3 blurred = tu1_2D_val.rgb;
+	
+	//vec3 final = orig * blurred;
+	float blurred_grey = (blurred.r+blurred.g+blurred.b)*0.333;
+	blurred = vec3(blurred_grey,blurred_grey,blurred_grey);
+	vec3 final = orig*(orig + 2.0*blurred*(1.0-orig)); //"OVERLAY"
 	gl_FragColor = vec4(final.r,final.g,final.b,0);
 }
---
+++ vdrift-data/shaders/hblur_rect/fragment.glsl
@@ -0,0 +1,41 @@
+uniform sampler2DRect tu0_2DRect;
+varying vec2 texcoord_2d;
+uniform float screenw;
+uniform float screenh;
+
+void main()
+{
+	vec2 tc = texcoord_2d;
+	tc.x *= screenw;
+	tc.y *= screenh;
+	
+	vec4 tu0_2D_val = texture2DRect(tu0_2DRect, tc);
+	
+	vec4 final = vec4(0.0, 0.0, 0.0, 0.0);
+
+	/*final += 0.015625 * texture2DRect(tu0_2DRect, tc + vec2(0.0, -3.0) );
+	final += 0.09375 * texture2DRect(tu0_2DRect, tc + vec2(0.0, -2.0) );
+	final += 0.234375 * texture2DRect(tu0_2DRect, tc + vec2(0.0, -1.0) );
+	final += 0.3125 * texture2DRect(tu0_2DRect, tc + vec2(0.0, 0.0) );
+	final += 0.234375 * texture2DRect(tu0_2DRect, tc + vec2(0.0, 1.0) );
+	final += 0.09375 * texture2DRect(tu0_2DRect, tc + vec2(0.0, 2.0) );
+	final += 0.015625 * texture2DRect(tu0_2DRect, tc + vec2(0.0, 3.0) );*/
+	
+	/*final += 0.015625 * texture2DRect(tu0_2DRect, tc + vec2(-3.0*d, 0.0) );
+	final += 0.09375 * texture2DRect(tu0_2DRect, tc + vec2(-2.0*d, 0.0) );
+	final += 0.234375 * texture2DRect(tu0_2DRect, tc + vec2(-1.0*d, 0.0) );
+	final += 0.3125 * texture2DRect(tu0_2DRect, tc + vec2(0.0, 0.0) );
+	final += 0.234375 * texture2DRect(tu0_2DRect, tc + vec2(1.0*d, 0.0) );
+	final += 0.09375 * texture2DRect(tu0_2DRect, tc + vec2(2.0*d, 0.0) );
+	final += 0.015625 * texture2DRect(tu0_2DRect, tc + vec2(3.0*d, 0.0) );*/
+	
+	const float samples = 8.0;
+	const float sinv = 1.0 / samples;
+	
+	for (float i = -4.0; i < 5.0; i++)
+	{
+		final += sinv * texture2DRect(tu0_2DRect, tc + vec2(i, 0.0) );
+	}
+	
+	gl_FragColor = final;
+}
