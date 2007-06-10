f6e832cf2b5d12549d59aa91811ad43cd41ba7d6
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2007-06-10 21:40:24 +0000

    Adjusted shaders for better looking bloom.  Added new simple skin button textures.

--- vdrift-data/shaders/blurpass/fragment.glsl
+++ vdrift-data/shaders/blurpass/fragment.glsl
@@ -21,9 +21,9 @@ void main()
 	vel2 = vel2 * blurfactor;
 	//vel2 = vel2 * 0.0;
 	vel2 = clamp(vel2, -0.1, 0.1);
+	//vel2 = vec2(0.1,0.0);
 	vel2.x *= screenw;
 	vel2.y *= screenh;
-	//vel2 = vec2(0.1,0.0);
 	
 	//vec2 t = vec2(0.5,screenratio_over2) + vec2(0.5,0.375) * (newpos2.xy); //TODO:  0.5,0.375 is related to the screen ratio, replace it w/ actual values passed from the CPU
 	vec2 screenover2 = vec2(screenw,screenh)*0.5;
@@ -33,19 +33,8 @@ void main()
 	//const float sm1inv = 1.0/(8.0-1.0); //= 1.0/(samples-1.0);
 	//const float w = 0.125; //= 1.0 / samples;
 	vec4 a = vec4(0.0,0.0,0.0,0.0);
-	/*float mynoise[8];
-	vec4 myn1 = texture2D(tu1_2D,(t)*10.0);
-	mynoise[0] = myn1.r;
-	mynoise[1] = myn1.g;
-	mynoise[2] = myn1.b;
-	mynoise[3] = myn1.a;
-	myn1 = texture2D(tu1_2D,(t)*10.0);
-	mynoise[0] = myn1.r;
-	mynoise[1] = myn1.g;
-	mynoise[2] = myn1.b;
-	mynoise[3] = myn1.a;*/
 	//float mynoise = texture2D(tu1_2D,t*10.0).r;
-	float mynoise = (texture2D(tu1_2D,t*5.0).r-0.5)*2.0;
+	//float mynoise = (texture2D(tu1_2D,t*5.0).r-0.5)*2.0;
 	//float mynoise = texture2D(tu1_2D,t).r;
 	//float mynoise = texture2D(tu1_2D,newpos2.xy*5.0).r;
 	
@@ -60,7 +49,8 @@ void main()
 		//float mynoise = texture2D(tu1_2D,(t+vec2(i,i))*5.0).r;
 		//float ds = (i+(mynoise-0.5)*2.0) / (samples - 1.0);
 		//float ds = (i+mynoise) / (samples - 1.0);
-		float ds = (i+mynoise) * (sm1inv);
+		//float ds = (i+mynoise) * (sm1inv);
+		float ds = i * sm1inv;
 		//a = a + texture2D(tu0_2D,t+vel2*ds-vel2*0.5)*w;
 		vec2 tc = t+vel2*(ds-0.5);
 		//tc.y = min(0.749,tc.y); //TODO:  0.749 is related to the screen ratio, replace it w/ actual values passed from the CPU
