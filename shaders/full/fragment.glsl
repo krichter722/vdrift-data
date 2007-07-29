51b1a2b94031a51cb5df269d6eccd9ef977ba48a
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2007-07-29 02:37:41 +0000

    Adjusted laguna seca to not flag any transparent objects since I want alpha testing for them all, now.  This fixes some bugs in the track rendering.  I also modified the "full" shader to do the cascaded shadow maps explained in the trunk rev 1786.

--- shaders/full/fragment.glsl
+++ shaders/full/fragment.glsl
@@ -2,8 +2,9 @@ varying vec2 texcoord_2d;
 varying vec3 normal;
 uniform sampler2D tu0_2D;
 uniform sampler2D tu1_2D;
-//uniform sampler2DShadow tu2_2D;
-uniform sampler2DRect tu2_2DRect;
+uniform sampler2DShadow tu2_2D;
+uniform sampler2DShadow tu3_2D;
+//uniform sampler2DRect tu2_2DRect;
 
 uniform float screenw;
 uniform float screenh;
@@ -16,59 +17,58 @@ varying vec4 ecpos;
 uniform vec3 lightposition;
 //varying vec3 halfvector;
 
+varying vec4 projshadow_0;
+varying vec4 projshadow_1;
+
+//uniform mat4 light_matrix_0;
+//uniform mat4 light_matrix_1;
+
 void main()
 {
+	vec3 shadowcoords[2];
+	shadowcoords[0] = projshadow_0.xyz;
+	shadowcoords[1] = projshadow_1.xyz;
+	//shadowcoords[0] = (light_matrix_0 * (gl_TextureMatrix[1] * (gl_ModelViewMatrix * gl_FragCoord))).xyz;
+	//shadowcoords[1] = (light_matrix_1 * (gl_TextureMatrix[1] * (gl_ModelViewMatrix * gl_FragCoord))).xyz;
+	float notshadow[2];
+	notshadow[0] = shadow2D(tu2_2D, shadowcoords[0]).r;
+	notshadow[1] = shadow2D(tu3_2D, shadowcoords[1]).r;
+	
+	const float bound = 1.0;
+	const float fade = 10.0;
+	float effect[2];
+	
+	for (int i = 0; i < 2; ++i)
+	//for (int i = 3; i < 4; ++i)
+	{
+		shadowcoords[i] = clamp(shadowcoords[i], 0.0, bound);
+		float xf1 = 1.0-min(1.0,shadowcoords[i].x*fade);
+		float xf2 = max(0.0,shadowcoords[i].x*fade-(fade-1.0));
+		float yf1 = 1.0-min(1.0,shadowcoords[i].y*fade);
+		float yf2 = max(0.0,shadowcoords[i].y*fade-(fade-1.0));
+		float zf1 = 1.0-min(1.0,shadowcoords[i].z*fade);
+		float zf2 = max(0.0,shadowcoords[i].z*fade-(fade-1.0));
+		effect[i] = max(xf1,max(xf2,max(yf1,max(yf2,max(zf1,zf2)))));
+		//notshadow[i] = max(notshadow[i],effect[i]);
+	}
+	
+	float notshadowfinal = notshadow[0];
+	notshadowfinal = mix(notshadowfinal,notshadow[1],effect[0]);
+	notshadowfinal = max(notshadowfinal,effect[1]);
+	/*notshadowfinal = mix(notshadowfinal,notshadow[1],effect[0]);
+	notshadowfinal = mix(notshadowfinal,notshadow[2],effect[1]);
+	notshadowfinal = max(notshadowfinal,effect[2]);*/
+	
+	
+	
+	
+	//float notshadow = texture2DRect(tu2_2DRect, gl_FragCoord.xy*0.5).r;
+	//float notshadow = texture2DRect(tu2_2DRect, gl_FragCoord.xy).r;
+	
 	vec3 normnormal = normalize(normal);
 	
 	vec4 tu0_2D_val = texture2D(tu0_2D, texcoord_2d);
 	vec4 tu1_2D_val = texture2D(tu1_2D, texcoord_2d);
-	
-	//float notshadow = texture2DRect(tu2_2DRect, gl_FragCoord.xy*0.5).r;
-	float notshadow = texture2DRect(tu2_2DRect, gl_FragCoord.xy*0.5).r;
-	//notshadow = (1.0-notshadow)*(tu0_2D_val.a);
-	//notshadow = tu0_2D_val.a;
-	//float notshadow = shadow2D(tu2_2D, projshadow).r;
-	/*float notshadow = 0.0;
-	const float ep = 0.000488;
-	vec3 projshadow3 = vec3(projshadow);
-	
-	//2X2 PCF
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(ep,ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(ep,-ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(-ep,-ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(-ep,ep,0.0)).r;
-	notshadow *= 0.25;*/
-	
-	//3X3 PCF
-	/*notshadow += shadow2D(tu2_2D, projshadow3+vec3(ep,ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(ep,0.0,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(ep,-ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(0.0,-ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(-ep,-ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(-ep,0.0,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(-ep,ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(0.0,ep,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3).r;
-	notshadow *= 0.11111111;*/
-	
-	/*//2X2 dithered PCF
-	vec2 o = mod(floor(gl_FragCoord.xy),2.0)*ep*2.0;
-	const float ep3 = ep*3.0;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(-ep3+o.x,ep3+o.y,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(ep+o.x,ep3+o.y,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(-ep3+o.x,-ep+o.y,0.0)).r;
-	notshadow += shadow2D(tu2_2D, projshadow3+vec3(ep3+o.x,-ep+o.y,0.0)).r;
-	notshadow *= 0.25;*/
-	/*const float bound = 1.0;
-	vec2 shadowcoords = projshadow.xy;
-	const float fade = 10.0;
-	shadowcoords.x = clamp(shadowcoords.x, 0.0, bound);
-	shadowcoords.y = clamp(shadowcoords.y, 0.0, bound);
-	float xf1 = 1.0-min(1.0,shadowcoords.x*fade);
-	float xf2 = max(0.0,shadowcoords.x*fade-(fade-1.0));
-	float yf1 = 1.0-min(1.0,shadowcoords.y*fade);
-	float yf2 = max(0.0,shadowcoords.y*fade-(fade-1.0));
-	notshadow = max(notshadow,max(xf1,max(xf2,max(yf1,yf2))));*/
 	
 	vec3 texcolor = tu0_2D_val.rgb;
 	vec3 ambient = texcolor;
@@ -76,7 +76,7 @@ void main()
 	float difdot = max(dot(normnormal,lightposition),0.0);
 	//notshadow *= min(difdot*10.0,1.0);
 	//notshadow *= 1.0-difdot;
-	difdot *= notshadow;
+	difdot *= notshadowfinal;
 	vec3 diffuse = texcolor*difdot;
 	
 	float gloss = tu1_2D_val.r;
@@ -92,7 +92,7 @@ void main()
 	
 	vec3 specular = vec3((pow(specval,128.0)*0.4+pow(specval,4.0)*0.2)*gloss);
 	
-	gl_FragColor.rgb = ambient*0.5 + diffuse*1.0 + specular*notshadow;
+	gl_FragColor.rgb = ambient*0.5 + diffuse*1.0 + specular*notshadowfinal;
 	
 	//gl_FragColor.rgb = diffuse;
 	//gl_FragColor.rgb = texture2DRect(tu2_2DRect, gl_FragCoord.xy).rgb;
