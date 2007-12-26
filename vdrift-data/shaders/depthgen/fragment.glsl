c7d06dca9b4c04e2041a05f183c80b87242125ec
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2007-12-26 19:59:21 +0000

    Switched from using OpenGL polygon offset to explicit depth offset in the depthgen shader for cleaning up shadow acne.

--- vdrift-data/shaders/depthgen/fragment.glsl
+++ vdrift-data/shaders/depthgen/fragment.glsl
@@ -1,5 +1,5 @@
 varying float lightdotnorm;
-uniform float depthoffset;
+//uniform float depthoffset;
 uniform sampler2D tu0_2D;
 varying vec2 texcoord;
 
@@ -10,14 +10,14 @@ void main()
 	//gl_FragDepth = gl_FragCoord.z+0.0009;
 	//gl_FragDepth = gl_FragCoord.z+0.007;
 	
-	vec4 tu0color = texture2D(tu0_2D, texcoord);
+	const vec4 tu0color = texture2D(tu0_2D, texcoord);
 	
 	/*if (tu0color.a < 0.1)
 		discard;
 	else*/
 	{
 		//gl_FragDepth = gl_FragCoord.z+depthoffset + (1.0-tu0color.a)*100.0;
-		gl_FragDepth = gl_FragCoord.z+depthoffset;
+		gl_FragDepth = gl_FragCoord.z+0.001;
 		gl_FragColor = tu0color;
 	}
 	
