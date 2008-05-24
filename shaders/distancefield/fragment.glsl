9d90cfb7b0f0222ae556c14b7cf175df444280ff
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2008-05-24 20:43:13 +0000

    Added anti-aliased distancefield rendering shader for fonts.

--- shaders/distancefield/fragment.glsl
+++ shaders/distancefield/fragment.glsl
@@ -6,7 +6,11 @@ void main()
 	// Setting Each Pixel To Red
 	//gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 	
-	gl_FragColor = texture2D(tu0_2D, tu0coord)*gl_Color;
+	vec4 texcolor = texture2D(tu0_2D, tu0coord);
+	const float width = 0.05;
+	texcolor.a = smoothstep(0.5-width, 0.5+width, texcolor.a);
+	
+	gl_FragColor = texcolor*gl_Color;
 	
 	//gl_FragColor.rg = tu0coord*0.5+0.5;
 	//gl_FragColor.ba = vec2(1.0);
