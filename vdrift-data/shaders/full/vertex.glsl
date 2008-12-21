04f866bb99780b6b547fcd71b2ccfdcd9f43c8d7
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2008-12-21 17:12:22 +0000

    Updated "full" shader and added #define options for shadows and reflection quality.

--- vdrift-data/shaders/full/vertex.glsl
+++ vdrift-data/shaders/full/vertex.glsl
@@ -1,18 +1,21 @@
-uniform vec3 lightposition;
-
-varying vec2 texcoord_2d;
-varying vec3 normal;
-varying vec3 viewdir;
+#ifdef _SHADOWS_
 varying vec4 projshadow_0;
 varying vec4 projshadow_1;
+#endif
+
+varying vec2 texcoord_2d;
+varying vec3 normal_eye;
+varying vec3 viewdir;
 
 void main()
 {
 	//transform the vertex
 	gl_Position = ftransform();
 	
+	#ifdef _SHADOWS_
 	projshadow_0 = gl_TextureMatrix[2] * (gl_TextureMatrix[1] * (gl_ModelViewMatrix * gl_Vertex));
 	projshadow_1 = gl_TextureMatrix[3] * (gl_TextureMatrix[1] * (gl_ModelViewMatrix * gl_Vertex));
+	#endif
 	
 	//set the color
 	gl_FrontColor = gl_Color;
@@ -20,11 +23,12 @@ void main()
 	//set the texture coordinates
 	texcoord_2d = vec2(gl_MultiTexCoord0);
 	
-	//set the normal, eyespace normal, and eyespace position
-	mat3 cam_rotation_inv = mat3(gl_TextureMatrix[1][0].xyz,gl_TextureMatrix[1][1].xyz,gl_TextureMatrix[1][2].xyz);
-	normal = (cam_rotation_inv * gl_NormalMatrix) * gl_Normal;
+	//compute the eyespace normal
+	normal_eye = gl_NormalMatrix * gl_Normal;
 	
-	vec3 worldvert = (gl_TextureMatrix[1] * (gl_ModelViewMatrix * gl_Vertex)).xyz;
-	vec3 campos = gl_TextureMatrix[1][3].xyz;
-	viewdir = worldvert - campos;
+	//compute the eyespace position
+	vec4 ecposition = gl_ModelViewMatrix * gl_Vertex;
+	
+	//compute the eyespace view direction
+	viewdir = vec3(ecposition)/ecposition.w;
 }
