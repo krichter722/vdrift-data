423d63ae26f8924abc636c20fa1ae7faa5cd5e9d
joevenzon <joevenzon@97628b5f-472e-0410-8e9b-d81ab590400b>
2009-01-01 00:24:45 +0000

    Depth offset now accounts for shadowmap resolution.

--- shaders/depthgen/fragment.glsl
+++ shaders/depthgen/fragment.glsl
@@ -8,7 +8,17 @@ void main()
 {
 	//float depthoffset = mix(0.01,0.0001,eyespacenormal.z*eyespacenormal.z);
 	//float depthoffset = mix(0.005,0.001,eyespacenormal.z);
+	
+#ifdef _SHADOWSLOW_
+	//float depthoffset = mix(0.01,0.002,eyespacenormal.z);
+	float depthoffset = mix(0.005,0.001,eyespacenormal.z);
+#else
+#ifdef _SHADOWSMEDIUM_
+	float depthoffset = mix(0.00375,0.00075,eyespacenormal.z);
+#else
 	float depthoffset = mix(0.0025,0.0005,eyespacenormal.z);
+#endif
+#endif
 	
 	gl_FragDepth = gl_FragCoord.z+depthoffset;
 	//vec4 tu0color = texture2D(tu0_2D, texcoord);
