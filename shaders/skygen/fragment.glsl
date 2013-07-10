b1d066ca1f49c5883157ac95f56a7266da92afce
log0 <log0@97628b5f-472e-0410-8e9b-d81ab590400b>
2013-07-10 00:41:01 +0000

    Try a simpler skygen shader.

--- shaders/skygen/fragment.glsl
+++ shaders/skygen/fragment.glsl
@@ -9,25 +9,26 @@ const float g = -0.990;
 
 // air, aerosol density scale (turbidity)
 //uniform vec2 RayleighMieScaleHeight;// = {0.25, 0.1};
-const vec2 RayleighMieScaleHeight = vec2(0.25, 0.1);
+//const vec2 RayleighMieScaleHeight = vec2(0.25, 0.1);
+const float ScaleHeight = 0.25; // rayleigh scale height
 
 // rgb wavelength(rayleigh and mie)
 //const vec3 WaveLength = vec3(0.65, 0.57, 0.475);
 const vec3 InvWavelength = vec3(5.602, 9.473, 19.644); // pow(WaveLength, -4.0);
 const vec3 WavelengthMie = vec3(1.435, 1.603, 1.869); // pow(WaveLength, -0.84);
 
-const float PI = 3.14159265;
 const float InnerRadius = 6356.7523142;
 const float OuterRadius = 6356.7523142 * 1.0157313;
 const float Scale = 1.0 / (6356.7523142 * 1.0157313 - 6356.7523142);
 
+//const float PI = 3.14159265;
 //const float ESun = 20.0;
 //const float Kr = 0.0025;
 //const float Km = 0.0010;
-const float KrESun = 0.0025 * 20.0;
-const float KmESun = 0.0010 * 20.0;
+const float KrESun = 0.0025 * 15.0;
+const float KmESun = 0.0015 * 15.0;
 const float Kr4PI = 0.0025 * 4.0 * 3.1415159;
-const float Km4PI = 0.0010 * 4.0 * 3.1415159;
+const float Km4PI = 0.0015 * 4.0 * 3.1415159;
 
 #define NumSamples 4
 
@@ -53,74 +54,55 @@ float HitOuterSphere(vec3 Pos, vec3 Dir)
    return t + q;
 }
 
-vec2 DensityRatio(float Height)
+// Sean O'Neil 2004
+float ScaleDepth(float ViewSunCos)
 {
-   float Altitude = (Height - InnerRadius) * Scale;
-   return exp(-Altitude / RayleighMieScaleHeight);
-}
-
-vec2 OpticalDepth(vec3 Pos, vec3 Dir, float Length)
-{
-   float SampleLength = Length / NumSamples;
-   float ScaledLength = SampleLength * Scale;
-   vec3 SampleRay = Dir * SampleLength;
-   Pos += SampleRay * 0.5;
-   
-   vec2 OpticalDepth = vec2(0);
-   for(int i = 0; i < NumSamples; i++)
-   {
-      float Height = length(Pos);
-      OpticalDepth += DensityRatio(Height);
-      Pos += SampleRay;
-   }
-   
-   return OpticalDepth * ScaledLength;
+   float x = 1.0 - ViewSunCos;
+   return ScaleHeight * exp(-0.00287 + x * (0.459 + x * (3.83 + x * (-6.80 + x * 5.25))));
 }
 
 vec3 Scatter(vec3 ViewPos, vec3 ViewDir, vec3 SunDir)
 {
+   float Height0 = length(ViewPos);
+   float Altitude0 = (Height0 - InnerRadius) * Scale;
+   float Depth0 = exp(-Altitude0 / ScaleHeight);
+   float ViewAngle0 = dot(ViewDir, ViewPos) / Height0;
+   float DepthOffset = Depth0 * ScaleDepth(ViewAngle0);
+
    float ViewLength = HitOuterSphere(ViewPos , ViewDir);
    float SampleLength = ViewLength / NumSamples;
    float ScaledLength = SampleLength * Scale;
    vec3 SampleRay = ViewDir * SampleLength;
    vec3 SamplePos = ViewPos + SampleRay * 0.5;
-   ViewLength -= SampleLength * 0.5;
-   
-   vec3 Rayleigh = vec3(0);
-   vec3 Mie = vec3(0);
+
+   vec3 Color = vec3(0.0, 0.0, 0.0);
    for(int i = 0; i < NumSamples; i++)
    {
-      float SunLength = HitOuterSphere(SamplePos, SunDir);
-      vec2 SunOpticalDepth = OpticalDepth(SamplePos, SunDir, SunLength);
-      vec2 ViewOpticalDepth = OpticalDepth(SamplePos, ViewDir, ViewLength);
-      
-      vec2 OpticalDepth = SunOpticalDepth + ViewOpticalDepth;
-      vec3 Attenuation = exp(-Kr4PI * InvWavelength * OpticalDepth.x - Km4PI * OpticalDepth.y);
-      
-      float SampleHeight = length(SamplePos);
-      vec2 DensityRatio = DensityRatio(SampleHeight) * ScaledLength;
-      
-      Rayleigh += DensityRatio.x * Attenuation;
-      Mie += DensityRatio.y * Attenuation;
-      
+      float Height = length(SamplePos);
+      float Altitude = (Height - InnerRadius) * Scale;
+      float Depth = exp(-Altitude / ScaleHeight);
+      float LightAngle = dot(SunDir, SamplePos) / Height;
+      float ViewAngle = dot(ViewDir, SamplePos) / Height;
+      float OpticalDepth = DepthOffset + Depth * (ScaleDepth(LightAngle) - ScaleDepth(ViewAngle));
+      vec3 Attenuate = exp(-OpticalDepth * (InvWavelength * Kr4PI + Km4PI));
+      Color += Attenuate * (Depth * ScaledLength);
       SamplePos += SampleRay;
-      ViewLength -= SampleLength;
    }
    
+   vec3 Ray = Color * KrESun * InvWavelength;
+   vec3 Mie = Color * KmESun * WavelengthMie;
    float ViewSunCos = -dot(ViewDir, SunDir);
    float ViewSunCos2 = ViewSunCos * ViewSunCos;
-   
-   Rayleigh = Rayleigh * KrESun * InvWavelength;
-   Mie = Mie * KmESun * WavelengthMie;
-   
-   return Rayleigh * RayleighPhase(ViewSunCos2) + Mie * MiePhase(ViewSunCos, ViewSunCos2);
+ 
+   return Ray * RayleighPhase(ViewSunCos2) + Mie * MiePhase(ViewSunCos, ViewSunCos2);
 }
 
 void main(void)
 {
-   vec3 ViewPos = vec3(0, 0, InnerRadius + 1e-3);
+   vec3 ViewPos = vec3(0, 0, InnerRadius + 0.5);
    vec3 ViewDir = normalize(vViewDirection);
    vec3 SunDir = normalize(uLightDirection);
    vec3 Color = Scatter(ViewPos, ViewDir, SunDir);
-   gl_FragColor = vec4(Color, 1.0f);
+   vec3 HDR = 1.0 - exp(Color * -2.0);
+   gl_FragColor = vec4(HDR, 1.0f);
 }
